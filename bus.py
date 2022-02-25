793feb10814c93267ca23fc5d56513c7d676fa10
Asianna Sample <99026080+Asianna01@users.noreply.github.com>
2022-02-25 23:13:20 +0000

    Did the homework problems to the best of my ability

--- bus.py
+++ bus.py
@@ -1,7 +1,40 @@
 #######################################################
-#
+#Asianna Sample HW 2
 # COSC 140 Homework 2, "bus trip planner" problem
 #
 #######################################################
+departure = input("What time will your bus depart? HHMM ")
+distance = input ("How far do you need to travel? (KM) ")
+stops = input ("How many stops before your destination? ")
+departure = int(departure)
+distance = int (distance)
+stops = int (stops)
 
+if departure <=2400:
+  #seperating the hours an minutes
+  hours = departure // 100
+  minutes = departure % 100
 
+  if minutes <= 59:
+  #Turning it into seconds in order to math it 
+    hours_insec = hours * 3600
+    min_insec = minutes * 60
+    total_sec = hours_insec + min_insec#Will need to subtract
+  
+  #The amount of time to get places 
+    time_stops = stops * 30 #30 because everything is in sec 
+    total_time = (distance / 40) * 3600 #40km/hour into sec 
+    arrival_time = total_sec + (time_stops + total_time)
+
+  #arrival_sec = (total_sec + time_stops)/ total_time 
+    final_hours = int(arrival_time % 86400)//3600
+    final_minutes = int(arrival_time % 3600)//60
+    final_sec = int(arrival_time % 60)
+
+    print (f" Your bus will arrive at {final_hours:02d}:{final_minutes:02d}:{final_sec:02d}")
+  else:
+    print("Minutes are not valid. Please enter something equal or less than 59.")
+  
+else:
+  print("Sorry, the time you have entered is not valid. Please try to enter an hour that is or less than 24. ")
+  
--- face.py
+++ face.py
@@ -1,8 +1,160 @@
 #######################################################
-#
+#Asianna Sample
 # COSC 140 Homework 2, "face" problem
 #
 #######################################################
-
 import turtle
+dumbo = turtle.Turtle()
+screen = dumbo.getscreen()
+screen.title("Picture")
+screen.setworldcoordinates(0, 0, 400, 400)
+dumbo.speed('fastest')
+dumbo.fillcolor("purple")
+ 
+# function for creation of eye
+def dcirc(col,rad):
+    dumbo.down()
+    dumbo.fillcolor(col)
+    dumbo.begin_fill()
+    dumbo.circle(rad)
+    dumbo.end_fill()
+    dumbo.up()
+ 
+ 
+# draw face
+dumbo.fillcolor('brown')
+dumbo.begin_fill()
+dumbo.circle(150)
+dumbo.end_fill()
+dumbo.up()
+ 
+# draw eyes
+dumbo.goto(-40, 120)
+dcirc('white', 15)
+dumbo.goto(-37, 125)
+dcirc('black', 5)
+dumbo.goto(40, 120)
+dcirc('white', 15)
+dumbo.goto(40, 125)
+dcirc('black', 5)
+ 
+# draw nose
+dumbo.goto(0, 80)
+dcirc('orange', 8)
+ 
+# draw face
+dumbo.goto(-40, 90)
+dumbo.down()
+dumbo.right(90)
+dumbo.circle(40, 180)
+dumbo.up()
+# draw face
+dumbo.down()
+dumbo.right(90)
+dumbo.circle(40, 180)
+dumbo.up()
+
+# draw face
+dumbo.goto(-40, 170)
+dumbo.down()
+# dumbo.right(90)
+dumbo.circle(40, 180)
+dumbo.up()
+
+
+#Eyebrows
+dumbo.goto(-60,150)
+dumbo.fillcolor('black')
+dumbo.pendown()
+dumbo.begin_fill()
+dumbo.forward(40) 
+dumbo.left(90)
+dumbo.forward(10) 
+dumbo.left(90) 
+dumbo.forward(40) 
+dumbo.left(90) 
+dumbo.forward(10) 
+dumbo.left(90)
+dumbo.end_fill()
+dumbo.penup()
+
+#Other eyebrow
+dumbo.goto(15,150)
+dumbo.pendown()
+dumbo.fillcolor('black')
+dumbo.pendown()
+dumbo.begin_fill()
+dumbo.forward(40) 
+dumbo.left(90)
+dumbo.forward(10) 
+dumbo.left(90) 
+dumbo.forward(40) 
+dumbo.left(90) 
+dumbo.forward(10) 
+dumbo.left(90)
+dumbo.end_fill()
+dumbo.penup()
+
+#ears 
+dumbo.goto(40,290)
+dumbo.pendown()
+dumbo.forward(100)
+dumbo.left(120)
+dumbo.forward(100)
+dumbo.left(120)
+dumbo.forward(100)
+dumbo.penup()
+
+#ear2
+dumbo.goto(-70,380)
+dumbo.pendown()
+dumbo.forward(100)
+dumbo.left(120)
+dumbo.forward(100)
+dumbo.left(120)
+dumbo.forward(100)
+
+
+
+
+
+
+
+# dumbo.goto(-40,89)
+
+ 
+
+screen.exitonclick()
+
+
+
+# def stararm():
+#   s = 100
+#   for i in range (70):
+#     dumbo.forward(s)
+#     dumbo.right(120)
+#     s = s - 3
+  
+
+
+# def main():
+#   dumbo.penup()
+#   dumbo.goto(0,90)
+#   dumbo.setposition(0,40)
+#   dumbo.pendown()
+#   stararm()
+#   dumbo.setposition(0,90)
+#   dumbo.left(180)
+#   stararm()
+  
+#   # turtle.setworldcoordinates(30, 50)
+
+#   screen.exitonclick()
+
+
+# main()
+
+
+
+
 
